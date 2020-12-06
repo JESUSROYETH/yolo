318608d5859601583949f0a3fe3408aa689b0a04
jesus <jesus.royeth@gmail.com>
2020-12-06 00:02:47 -0300

    agregado filtro para remover imagenes con menos de n detecciones

--- segmentar.sh
+++ segmentar.sh
@@ -33,6 +33,39 @@ else
 	     fi
 	     neurona=$4
 	     ./darknet detector test $(ls $neurona/*.data) $(ls $neurona/*.cfg) $(ls $neurona/*.weights) -thresh $threshi -dont_show -save_labels < $salida/train.txt
+	     if [ ! -z "$6" ]
+               then
+                  	mkdir $salida/basura
+			mkdir $salida/negativos
+			while read archivo; do
+			  echo "$archivo"
+			  archivo2=$(echo $archivo | sed -e "s/.jpg/.txt/g")
+			  s=$(wc -l < $archivo2)
+			  echo $s
+			  if  [ $s -eq 0 ];
+			  then
+			      echo "es  0"
+			      mv $archivo $salida/negativos     
+      			      mv $archivo2 $salida/negativos  
+
+			  else
+			   if  [ $s -lt $6 ];
+			      then 
+			   echo "es  menor que $6"
+ 			      mv $archivo $salida/basura     
+      			      mv $archivo2 $salida/basura  
+
+
+			  fi
+			  fi
+
+			done <$salida/train.txt
+
+             fi
+
 	fi
+
 fi
+
+
 
