303372deff94737b298df9c798b8f5df8f65f7be
jesus <jesus.royeth@gmail.com>
2020-12-06 03:05:16 -0300

    agregado amazon y corregido error de carpetas *

--- segmentar.sh
+++ segmentar.sh
@@ -19,9 +19,14 @@ else
 	for entry in $1/*.avi $1/*.mp4;
 	do
 	  echo $entry
-	  mkdir $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')
+          echo "$(basename $entry)" | cut -f 1 -d '.'
+          if [ -e "$entry" ]
+          then
+          mkdir $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')
 	  Yolo_mark/yolo_mark $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.') cap_video $entry $frames
 	  ls $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')/*.jpg>>$salida/train.txt
+          fi
+	  
 	done
 	if [ ! -z "$4" ]
 	   then
@@ -55,13 +60,56 @@ else
  			      mv $archivo $salida/basura     
       			      mv $archivo2 $salida/basura  
 
-
 			  fi
 			  fi
 
 			done <$salida/train.txt
 
+
              fi
+	     if [ ! -z "$7" ]
+               then
+               preetiquetar=$7
+	     else
+		preetiquetar=0
+             fi 
+
+          for entry in $1/*.avi $1/*.mp4;
+		do
+		  echo $entry
+                  if [ -e "$entry" ]
+                  then
+		  archivos=$salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')/*.jpg
+                  cantidad=$(ls $archivos | wc -l)
+                  echo $cantidad
+                  if  [ ! $cantidad -eq 0 ];
+		    then
+                  contar=0
+                  for textos in $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')/*.txt ;
+                  do
+                  contar=$(($contar + $(wc -l < $textos)))   
+                  done  
+                   if  [ $preetiquetar -eq 1 ]; 
+		   then      
+		       zip $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')__"$contar"__"$cantidad".zip $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')/*.jpg $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')/*.txt           
+		   else
+                      zip $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')__"$contar"__"$cantidad".zip $salida/$(echo "$(basename $entry)" | cut -f 1 -d '.')/*.jpg
+	 
+		   fi
+                   fi      
+                     
+                  fi
+                  
+		  
+	     done  
+             if [ ! -z "$8" ]
+               then
+             m=$(date +'%F-%H%M%S')
+               aws s3api put-object --bucket ftp-lythium --key "$salida"__$m/
+               aws s3 cp "$salida"/ s3://ftp-lythium/"$salida"__$m/ --recursive --exclude "*" --include "*.zip" --acl public-read
+             fi             
+            
+             
 
 	fi
 
